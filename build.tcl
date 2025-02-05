0be2fb36cae4da70c458e4a0f86e38fc046cf905
Robert Burnett <dr.slate2@gmail.com>
2025-02-05 10:07:22 -0600

    added build script

--- build.tcl
+++ build.tcl
@@ -1,2 +1,14 @@
-set out [source ccode.tcl]
-puts $out
+#!/usr/bin/env tclsh
+
+set builddir ".build"
+set cfile "$builddir/out.c"
+set exe "$builddir/out"
+set src ccode.tcl
+
+file mkdir $builddir
+set csrc [exec tclsh $src]
+set fd [open $cfile "w"]
+puts $fd $csrc
+close $fd
+exec cc $cfile -o $exe
+puts [exec $exe]
